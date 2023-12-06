fd799de52246cdeebb3b3da0dfd00efa7fe6db14
brodyedwardrichards <brody.richards@edu.sait.ca>
2023-12-05 21:33:03 -0700

    add buttonAlert function + styles + html id

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-//FOOTER - Add updating year
+// FOOTER - Add updating year
 
 const getYear = new Date();
 let year = getYear.getFullYear();
 
 document.getElementById("yearID").innerHTML = year;
 
+
+// Alert Button , display an alert message when button is pressed
+
+function buttonAlert() {
+    alert('Hello world!');
+}
+document.getElementById('btn-alert').onclick = buttonAlert;
